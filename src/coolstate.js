7a5a58ef1964eca700577e4c4d82ca9a401a601a
Alejandro G <mierdeneitor@gmail.com>
2018-05-01 00:41:40 +0200

    create coolstate

---
+++ src/coolstate.js
@@ -0,0 +1,19 @@
+void function($){
+	$.fn.extend({
+		coolState: function() {
+			var arg = arguments;
+			if(arg[1] != undefined) {
+				this.each(function(e,r) {
+					$(r).attr("data-" + arg[0], arg[1]);
+				});
+				return this;
+			} else if(arg[0] != undefined) {
+				var a = [];
+				this.each(function(e,r) {
+					a[e] = $(r).attr("data-" + arg[0]);
+				});
+				return a;
+			}
+		}
+	});
+}(jQuery)
