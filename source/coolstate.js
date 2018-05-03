fed530115f37f3a8ef58289df1350d4fa07c10b7
Alejandro G <mierdeneitor@gmail.com>
2018-05-03 15:49:21 +0200

    bump 0.1.7

--- source/coolstate.js
+++ source/coolstate.js
@@ -21,26 +21,3 @@
 	});
 }));
 
-
-void function($){
-  $.fn.extend({
-    coolState: function() {
-      var arg = arguments;
-      if(arg[1] != undefined) {
-        this.each(function(e,r) {
-          $(r).attr("data-" + arg[0], arg[1]);
-        });
-
-        return this;
-        
-      } else if(arg[0] != undefined) {
-        var a = [];
-        this.each(function(e,r) {
-          a[e] = $(r).attr("data-" + arg[0]);
-        });
-
-        return (a == [] || a[0] == undefined) ? undefined : a;
-      }
-    }
-  });
-}(jQuery)
