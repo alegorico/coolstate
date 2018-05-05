e81a39e7784b62a0a011fea72f9140bfb10dca47
Alejandro G <mierdeneitor@gmail.com>
2018-05-05 23:27:12 +0200

    Update uglify.js

--- tasks/uglify.js
+++ tasks/uglify.js
@@ -1,6 +1,7 @@
 module.exports = {
   uglify: {
     options: {
+        banner: '/*! MIT License \r\n Copyright (c) <%= grunt.template.today("yyyy") %> Alejandro G. \r\n */ ',
         mangle: {
           reserved: ['jQuery']
         }
